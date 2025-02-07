7cd7d4d0288db3fa8e3011a35aded218eea402ec
Stefan Stegmüller <stefan.stegmueller@de-cix.net>
2025-02-07 09:44:23 +0100

    adapt neo-tree keys

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,18 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<leader>e'] = 'close_window',
+          ['l'] = 'open', -- Expand folder or open file
+          ['h'] = 'close_node', -- Collapse folder
         },
       },
     },
