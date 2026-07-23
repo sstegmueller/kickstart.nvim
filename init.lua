55fc98910b2e0467645801488ee9b8c7727361f6
Stefan Stegmüller <stefan.stegmueller@de-cix.net>
2026-07-23 09:16:59 +0200

    rm unnessary imports

--- init.lua
+++ init.lua
@@ -840,13 +840,6 @@ require('lazy').setup({
       --
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'php-cs-fixer',
-        'prettier',
-        'prettierd',
-      })
-
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       vim.lsp.config('phpactor', servers['phpactor'])
